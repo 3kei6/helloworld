59584eca09d5940025757a3d0ba5cc4413e10acb
3kei6 <109353277+3kei6@users.noreply.github.com>
2022-11-13 13:11:35 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() //main函数，用于实现输出以换行结束的Hello World!
+{
+    cout << "Hello World!" << endl; //输出以换行结束的Hello World!
+
+    return 0; //返回整型0，此时main函数执行结束
+}
